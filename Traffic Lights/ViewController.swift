9a068ca980dc00368e6c6282363b9b41b3bf2c9b
Mertcan Akyürek <mertcan.akyurek@vngrs.com>
2024-04-10 10:29:31 +0300

    Initial Commit

---
+++ Traffic Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Lights
+//
+//  Created by Can Akyürek on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
