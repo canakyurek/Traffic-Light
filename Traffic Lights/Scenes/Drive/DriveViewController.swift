3cbdc5cacfe4abe963cf1ddd1e62f66151e97537
Mertcan Akyürek <mertcan.akyurek@vngrs.com>
2024-04-10 13:27:53 +0300

    Implemented TrafficLightView as a standalone component.

--- Traffic Lights/Scenes/Drive/DriveViewController.swift
+++ Traffic Lights/Scenes/Drive/DriveViewController.swift
@@ -18,20 +18,35 @@ class DriveViewController: BaseViewController {
         $0.numberOfLines = 2
     }
     
+    lazy var lightView = TrafficLightView()
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [brandLabel, lightView]).configure {
+        $0.axis = .vertical
+        $0.spacing = 16
+        $0.alignment = .center
+        $0.distribution = .fill
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         brandLabel.text = interactor?.brand ?? ""
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        lightView.stopAnimating()
+    }
+    
     override func setupView() {
         super.setupView()
         
         title = Constants.DriveView.title
-        view.addSubview(brandLabel)
-        brandLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(24)
-            make.leading.trailing.equalToSuperview().inset(16)
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(view.safeAreaLayoutGuide).inset(64)
+            make.leading.trailing.equalToSuperview().inset(24)
         }
     }
 }
